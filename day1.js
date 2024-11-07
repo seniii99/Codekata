8ccc6a93129d4a2fe3ae80ffaaa28b2eaa36b000
seniii99 <restarz@naver.com>
2024-11-07 10:17:24 +0900

    1회차

---
+++ day1.js
@@ -0,0 +1,13 @@
+// 두 수의 차
+
+// raw code
+
+// function solution(num1, num2) {
+//     var answer = 0;
+//     return answer;
+// }
+
+function solution(num1, num2) {
+    const answer = num1 -num2;
+    return answer;
+}
