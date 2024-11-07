7b2e2b31eebc6031721978dbf5b5303887341121
seniii99 <restarz@naver.com>
2024-11-07 10:36:22 +0900

    3회차

---
+++ day3.js
@@ -0,0 +1,12 @@
+// 몫 구하기
+
+// 예제 코드
+// function solution(num1, num2) {
+//     var answer = 0;
+//     return answer;
+// }
+
+function solution(num1, num2) {
+    const answer = Math.floor(num1 / num2);
+    return answer;
+}
