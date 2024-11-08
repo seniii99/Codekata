ab868c3de1b28f833ff99b0d80354f2e9b676efe
seniii99 <restarz@naver.com>
2024-11-08 12:19:29 +0900

    추가

---
+++ day11_20/day15.js
@@ -0,0 +1,17 @@
+// 나머지가 1이 되는 수 찾기
+// 자연수 n이 매개변수로 주어집니다. n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요. 답이 항상 존재함은 증명될 수 있습니다.
+
+// 예제 코드
+// function solution(n) {
+//     var answer = 0;
+//     return answer;
+// }
+
+function solution(n) {
+  for (let x = 2; x <= n; x++) {
+    if (n % x === 1) {
+      return x;
+    }
+  }
+  return n - 1;
+}
